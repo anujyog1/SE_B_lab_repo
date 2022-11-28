d9ab5781266256cbc2b5e033e9051c30525adee0
anujyog1 <agyogita@gmail.com>
2022-11-28 23:20:35 +0530

    mad

---
+++ src/main/java/SEGroup/JavaMavenJenkins/App.java
@@ -0,0 +1,13 @@
+package SEGroup.JavaMavenJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Java Maven Jenkins pipeline 28 th  Nov" );
+    }
+}
